79029e4b3d45bc1e1e315cc3ffa96e3216c147d8
Claus Bayer <claus.bayer@gmail.com>
2018-01-14 15:47:13 +1300

    Docs improved

--- doc/Install.md
+++ doc/Install.md
@@ -1,3 +1,4 @@
+
 [![Build Status](https://travis-ci.org/clausnz/php-helpers.svg?branch=master)](https://travis-ci.org/clausnz/php-helpers)
 
 # Install
--- doc/bin/create-function-doc.php
+++ doc/bin/create-function-doc.php
@@ -3,22 +3,27 @@
 define('PROJECT_DIR', __DIR__ . '/../../');
 require PROJECT_DIR . 'vendor/autoload.php';
 
+/*
+ * Generate "Table of Content" for global functions.
+ */
+
+// define some vars
 $helperFile = PROJECT_DIR . "src/helpers.php";
-$mdFile = PROJECT_DIR . "doc/Table_of_Content_Functions.md";
+$tocFile = PROJECT_DIR . "doc/Table_of_Content_Functions.md";
 $helperContent = file_get_contents($helperFile);
 
+// get group name from functions file
 $leftGroup = "@group(";
 $leftGroupEnd = "@endgroup(";
 $rightGroup = ")";
 $groupNames = str_between($leftGroup, $rightGroup, $helperContent);
 
-$md = fopen($mdFile, 'w+');
+// write header of ToC to file
+$tocContent = "# Available PHP Functions\n\n";
+$tocContent .= "## Table of Contents\n\n";
 
-fwrite($md, "# Available PHP Functions\n\n");
-fwrite($md, "## Table of Contents\n\n");
-
+// create ToC for functions and write to file
 $functionCounter = 0;
-
 foreach ($groupNames as $key => $groupName) {
     $leftContent = $leftGroup . $groupName . $rightGroup;
     $rightContent = $leftGroupEnd . $groupName . $rightGroup;
@@ -26,28 +31,27 @@ foreach ($groupNames as $key => $groupName) {
     $right = "'))";
     $functionNames = str_between($left, $right, str_between($leftContent, $rightContent, $helperContent)[0]);
 
-    fwrite($md, "* [$groupName](#$functionNames[0])\n");
+    $tocContent .= "* [$groupName](#$functionNames[0])\n";
     foreach ($functionNames as $functionName) {
-        fwrite($md, "    * [$functionName](#$functionName)\n");
+        $tocContent .= "    * [$functionName](#$functionName)\n";
         $functionCounter++;
     }
 }
+file_put_contents($tocFile, $tocContent);
 
-fclose($md);
+/*
+ * Generate "About" section.
+ */
 
 // write number of functions to file
 $fileNumberFunctions = PROJECT_DIR . "tmp/NR_FUNCTIONS";
 file_put_contents($fileNumberFunctions, $functionCounter);
 
-
 // write About.md
 $fileAbout = PROJECT_DIR . "doc/About.md";
-$fa = fopen($fileAbout, 'w+');
 
 $fileAboutContent = "# About\n\n";
 $description = "A Collection of :number useful php helper functions.\n";
 $fileAboutContent .= str_insert([':number' => file_get_contents($fileNumberFunctions)], $description);
 
-fwrite($fa, $fileAboutContent);
-
-fclose($fa);
+file_put_contents($fileNumberFunctions, $fileAboutContent);
